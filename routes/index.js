3b9fc503bfa593545dc3e451506065c7c6c79cca
Adrián Ramón <adrianramon2003@gmail.com>
2023-01-27 10:16:53 +0100

    Añadido tailwindCSS al proyecto

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/books', function(req, res, next) {
+  res.render('index', { title: 'Libros' });
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('index', { title: 'Añadir libro' });
 });
 
 module.exports = router;
