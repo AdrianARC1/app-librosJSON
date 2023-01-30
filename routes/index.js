81cea196e77427b31160bc88da2d88e36ecf36a0
Adrian <adrianramon2003@gmail.com>
2023-01-30 23:23:25 +0100

    Conseguido mandar datos json

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+let libros = []
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/books', function(req, res, next) {
-  res.render('books', { title: 'Libros' });
+  res.render('books', { libros
+    // titulo: libros.title,
+    // autor: libros.autor,
+    // imagen: libros.imagen,
+    // descripcion: libros.descrip,
+});
 });
 
 router.get('/add', function(req, res, next) {
   res.render('create', { title: 'Añadir libro' });
 });
 
+router.post('/books', function(req, res, next) {
+  const { title, autor, imagen, descrip } = req.body
+
+  if(!title || !autor || !imagen || !descrip){
+    res.status(400).send("No te dejes nada vacío")
+  }
+  let nuevoLibro={
+    title, autor, imagen, descrip
+  }
+  libros.push(nuevoLibro)
+  console.log(libros)
+
+});
 module.exports = router;
